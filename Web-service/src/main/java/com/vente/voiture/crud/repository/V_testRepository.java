d6c682e191735a4b0c077d616c795df2573367be
Johananjaratiana <118510387+Johananjaratiana@users.noreply.github.com>
2024-01-14 05:55:34 +0300

    Temp

--- Web-service/src/main/java/com/vente/voiture/crud/repository/V_testRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vente.voiture.crud.repository;
-
-import com.vente.voiture.crud.model.V_test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface V_testRepository extends JpaRepository<V_test, Long> {
-}
